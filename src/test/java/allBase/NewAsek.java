e0ac57b5278da2a678c58d01807446069e0426de
mdshamim <mdshamim3134@gmail.com>
2021-01-31 15:08:55 -0500

    bangladesh

--- src/test/java/allBase/NewAsek.java
+++ src/test/java/allBase/NewAsek.java
@@ -1,9 +1,14 @@
 package allBase;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class NewAsek {
-    public void a() throws InterruptedException {
+    public void a() throws InterruptedException, MalformedURLException {
         System.out.println(20);
         Thread.sleep(300);
+        URL url=new URL("");
+                
 
     }
 }
